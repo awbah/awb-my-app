b8a6a375c4f05d4c4582287bd2085d6ee38054db
Engr. Wurie <amaduwuriebah3761@gmail.com>
2025-08-20 19:36:38 +0000

    My first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  const name = 'Mariama'
+
+
+  return (
+    <>
+     <h1>Hello {name}</h1>
+    </>
+  )
+}
+
+export default App
